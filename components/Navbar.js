6e7d3134ae6cf53aea6557b7916615c82a20a6ef
andree-ro <andreerm14@gmail.com>
2026-08-08 21:32:30 -0600

    feat: agrega estructura inicial del proyecto y componente Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+// función en js que se llama navbar, y se marca como lo principal de este archivo
+// los nombres de funciones en react siempre se ponen en Mayuscula
+export default function Navbar(){ 
+    // retornara todo lo que este aquí
+    return (
+        // el # nos lleva a un punto especifico de la página
+        <nav>
+            <a href = "#top"> 
+                andree@dev<span>:~</span>
+            </a>
+        </nav>
+    );
+}
